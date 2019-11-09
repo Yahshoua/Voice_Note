1994d5c0c3e6ab1b15a219376037f79b11dfcf37
Anthony Otha <yahanthony8@gmail.com>
2019-11-10 00:15:09 +0100

    ajout methods

--- src/app/pop-over/pop-over.page.ts
+++ src/app/pop-over/pop-over.page.ts
@@ -1,5 +1,6 @@
+import { ServiceVoiceManagerService } from './../service-voice-manager.service';
 import { Component, OnInit } from '@angular/core';
-
+import { NavParams, PopoverController } from '@ionic/angular';
 @Component({
   selector: 'app-pop-over',
   templateUrl: './pop-over.page.html',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PopOverPage implements OnInit {
 
-  constructor() { }
+  constructor(public navParams: NavParams, public service: ServiceVoiceManagerService,public popOver: PopoverController ) { }
 
   ngOnInit() {
+   
   }
-
+  dismissModal() {
+    this.popOver.dismiss({
+      component : PopOverPage,
+      'dismissed': true
+    });
+  }
+  delete() {
+     let id = this.navParams.data.paramID;
+     this.service.delete(id)
+     setTimeout(()=> {
+        this.dismissModal()
+     }, 1000)
+  }
 }
